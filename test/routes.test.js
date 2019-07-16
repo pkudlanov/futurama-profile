6cd9b01312a2e29ee6507836767b6e660a25c865
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-15 18:42:43 -0700

    POST route test

--- test/routes.test.js
+++ test/routes.test.js
@@ -4,6 +4,13 @@ const app = require('../lib/app');
 describe('testing all the routes', () => {
     it('creates a profile with POST', () => {
         return request(app)
-            .post('/api/v1/');
+            .post('/api/v1/futurama')
+            .send({ name: 'Pavel Kudlanov', character: 'Bender' })
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'Pavel Kudlanov',
+                    character: 'Bender'
+                });
+            });
     });
 });
