08f3aa708a934e87902e44e0f753920dc8d153cf
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-15 18:48:47 -0700

    renaming from user to profile

--- test/routes.test.js
+++ test/routes.test.js
@@ -4,7 +4,7 @@ const app = require('../lib/app');
 describe('testing all the routes', () => {
     it('creates a profile with POST', () => {
         return request(app)
-            .post('/api/v1/futurama')
+            .post('/api/v1/profile')
             .send({ name: 'Pavel Kudlanov', character: 'Bender' })
             .then(res => {
                 expect(res.body).toEqual({
